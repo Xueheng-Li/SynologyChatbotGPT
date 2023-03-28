610cdcbe6186edda7d8af5b214180049494cbcc4
Xueheng-Li <61861637+Xueheng-Li@users.noreply.github.com>
2023-03-29 01:10:12 +0800

    Get up-to-date info from Bing and Google

--- settings.py
+++ settings.py
@@ -17,13 +17,11 @@ PORT = 5008
 
 
 # This system prompt sets up the character of the chatbot; change it if you want
-chatbot_character = '''You are a AI assistant, a friend of mine, trying to help me and my family as much as possible and in whatever ways you can. 
-    If the user talks to you in English, you respond in English. If the user talks to you in Chinese, you respond in Chinese. 
-    Be talkative, personable, friendly, positive, and speak always with love.'''
+chatbot_character = '''你是全能君，一名智能助手。你的使命是尽可能地用详尽的、温暖的、友善的话语帮助我和我的家人，在各种方面提供帮助和支持。无论我需要什么帮助或建议，你都会尽力提供详尽信息。'''
 
 
 # Set maximum conversation exchanges or idle time gap to start a new conversatoin
-max_conversation_length = 20 
+max_conversation_length = 20
 max_time_gap = 15 # minutes
 
 
@@ -38,3 +36,23 @@ image_size = "medium" # "small", "medium" or "large"
 # Set the virtual Python environment path if you want the bot to run Python code -- ignore it if you don't know what it is.
 # This variable is used to store the path to the virtual Python environment for the project. This virtual environment is where the project's dependencies are installed and managed, ensuring that they don't interfere with other projects or the system's global Python installation. By specifying the path to the virtual environment, the project can reference and activate the environment when needed, ensuring that the correct dependencies are used during execution.
 venv_path = "./venv"
+
+
+PROMPT_TEMPLATE = """
+Context information is below. Please refer to the context by using [number] notation when providing a response.
+---------------------
+{context_str}
+---------------------
+Current date: {current_date}.
+Using the provided context information, write a comprehensive reply to the given query.
+If the provided information refers to multiple subjects with the same name, write separate answers for each subject.
+
+首先，不考虑提供的背景信息，请回答以下问题（用中文）: {query_str}
+
+现在，考虑到提供的背景信息，这是否改变了原始答案？如果是，请用中文提供更新后的答案。如果没有，请用中文重复原始答案（If not, please repeat the original answer in 中文 (Chinese).）。无论如何，你都要提供一个回答。
+"""
+
+bing_key = "your_key_for_Bing_search" # get your free bing search api key here: https://learn.microsoft.com/en-us/bing/search-apis/bing-web-search/overview
+serpapi_key = "your_serpapi_key_for_google_search" # get your free key here: https://serpapi.com/
+serpapi_endpoint = "https://serpapi.com/search"
+
